5d363b9e1454d2bf9d59a7b986c7664ef06f1a7e
Melissa Christman <melchrstmn@gmail.com>
2019-11-03 20:55:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,10 +9,15 @@ class CashRegister
   end
   
   def add_item(title, amount, quantity=1)
-      self.total += amount * quantity
-      quantity.times {items << title}
-      self.last_transaction = amount * quantity
-    end
+    self.total += amount * quantity
+    quantity.times {items << title}
+    self.last_transaction = amount * quantity
+  end
     
+  def apply_discount
+    
+  def void_last_transaction
+    self.total = self.total - self.last_transaction
+  end
   
 end
